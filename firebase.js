630b7436983c0ef882ad2f9f3777b516c88d7aab
Agasta04 <agastapratamaump@gmail.com>
2025-04-19 23:55:39 +0700

    remove firebase initialization code

--- firebase.js
+++
@@ -1,9 +0,0 @@
-const admin = require("firebase-admin");
-
-const serviceAccount = JSON.parse(process.env.FIREBASE_CONFIG); // Ambil dari environment variable
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
-
-module.exports = admin;
